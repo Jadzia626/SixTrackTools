bd5b399f6983695ecf63570a4713dfa79321ebdf
Veronica K. B. Olsen <code@jadzia626.net>
2017-08-14 17:08:37 +0200

    Working version of STDump class

--- sttools/stdump.py
+++ sttools/stdump.py
@@ -20,9 +20,13 @@ class STDump:
     
     fileName  = None
     metaData  = None
-    varNames  = None
-    varTypes  = None
+    colNames  = None
+    colTypes  = None
+    colLabels = None
+    turnIdx   = None
+    partIdx   = None
     Data      = None
+    fData     = None
 
     nLines    = None
 
@@ -30,57 +34,62 @@ class STDump:
 
         self.fileName = fileName
         self.metaData = {}
-        self.varNames = []
-        self.varTypes = []
-        self.Data     = {}
-        
+        self.turnIdx  = {}
+        self.partIdx  = {}
         self.nLines   = 0
 
         # Read File
         with open(fileName,mode="rt") as tfsFile:
 
+            # Metadata
+            tfsLine = tfsFile.readline().strip()
+            if tfsLine[0] == "#":
+                clLine = tfsLine[1:].strip()
+                if clLine[0:2] == "DU":
+                    metaBits = clLine.split(",")
+                    self.metaData["FORMAT"] = metaBits[0][-1]
+                    for metaBit in metaBits:
+                        metaBit   = metaBit.strip()
+                        metaParts = metaBit.split("=")
+                        if metaParts[0] == "bez":                 self.metaData["BEZ"]         = str(metaParts[1].strip())
+                        if metaParts[0] == "number of particles": self.metaData["N_PART"]      = int(metaParts[1].strip())
+                        if metaParts[0] == "dump period":         self.metaData["DUMP_PERIOD"] = int(metaParts[1].strip())
+                        if metaParts[0] == "first turn":          self.metaData["FIRST_TURN"]  = int(metaParts[1].strip())
+                        if metaParts[0] == "last turn":           self.metaData["LAST_TURN"]   = int(metaParts[1].strip())
+                        if metaParts[0] == "HIGH":                self.metaData["HIGH"]        = str(metaParts[1].strip())
+                        if metaParts[0] == "FRONT":               self.metaData["FRONT"]       = str(metaParts[1].strip())
+            else:
+                logger.error("First line is not metadata")
+                return
+
+            # Set Columns from Dump Format
+            if self.metaData["FORMAT"] == "2":
+                self.colNames  = ["ID", "TURN","S",    "X",    "XP",      "Y",    "YP",      "Z",    "E",      "KTRACK"]
+                self.colTypes  = ["int","int", "float","float","float",   "float","float",   "float","float",  "int"]
+                self.colLabels = ["ID", "Turn","s[m]", "x[mm]","xp[mrad]","y[mm]","yp[mrad]","z[mm]","dE/E[1]","ktrack"]
+                self.Data      = {dKey:[] for dKey in self.colNames}
+            else:
+                logger.error("Unsupported data format")
+                return
+
+            # Read Data
             for tfsLine in tfsFile:
 
                 tfsLine = tfsLine.strip()
+                if tfsLine[0] == "#": continue
 
-                # Metadata
-                if tfsLine[0] == "#":
-                    clLine = tfsLine[1:].strip()
-                    logger.debug("Metadata %s" % clLine)
+                spLines = tfsLine.split()
+                for (spLine,cN) in zip(spLines,self.colNames):
+                    if len(spLines) == 10:
+                        pPart = int(spLines[0])
+                        pTurn = int(spLines[1])
+                        self.Data[cN].append(spLine)
 
-                    #~ spLines = tfsLine.split(",")[1:]
-                    
-                    #~ if   spLines[1][-1] == "d":
-                        #~ self.metaData[spLines[0]] = int(spLines[2])
-                    #~ elif spLines[1][-1] == "e":
-                        #~ self.metaData[spLines[0]] = float(spLines[2])
-                    #~ elif spLines[1][-1] == "s":
-                        #~ self.metaData[spLines[0]] = self.stripQuotes(spLines[2])
-                    #~ else:
-                        #~ logger.error("Unknown type '%s' for metadata variable '%s'" % (spLines[1], spLines[2]))
-                        #~ return False
+                dataID = len(self.Data["ID"]) -1
+                self.turnIdx.setdefault(pTurn,[]).append(dataID)
+                self.partIdx.setdefault(pPart,[]).append(dataID)
 
-                #~ # Header/Variable Names
-                #~ elif tfsLine[0] == "*":
-                    #~ spLines = tfsLine.split()[1:]
-                    #~ for spLine in spLines:
-                        #~ self.varNames.append(spLine)
-                        #~ self.Data[spLine] = []
-
-                #~ # Header/Variable Type
-                #~ elif tfsLine[0] == "$":
-                    #~ spLines = tfsLine.split()[1:]
-                    #~ for spLine in spLines:
-                        #~ self.varTypes.append(spLine)
-
-                #~ # Data
-                #~ else:
-                    #~ spLines = tfsLine.split()
-                    #~ for (spLine,vN,vT) in zip(spLines,self.varNames,self.varTypes):
-                        #~ self.Data[vN].append(spLine)
-                        #~ if vT == "%s":
-                            #~ self.Data[vN][-1] = self.stripQuotes(self.Data[vN][-1])
-                    #~ self.nLines += 1
+            self.nLines = len(self.Data["ID"])
 
             logger.info("%d lines of data read" % self.nLines)
 
@@ -92,30 +101,62 @@ class STDump:
         Convert data to NumPy arrays
         """
 
-        for i in range(len(self.varNames)):
-
-            vN = self.varNames[i]
-            vT = self.varTypes[i]
-            
-            if   vT[-1] == "d":
-                self.Data[vN] = np.asarray(self.Data[vN],dtype="int")
-            elif vT[-1] == "e":
-                self.Data[vN] = np.asarray(self.Data[vN],dtype="float")
-            elif vT[-1] == "s":
-                self.Data[vN] = np.asarray(self.Data[vN],dtype="str")
-            else:
-                logger.error("Unknown type '%s' for variable '%s'" % (vT, vN))
-                return False
+        for i in range(len(self.colNames)):
+            cN = self.colNames[i]
+            cT = self.colTypes[i]
+            self.Data[cN] = np.asarray(self.Data[cN],dtype=cT)
 
         return True
 
+
+    def filterPart(self, partID):
+        """
+        Select all particles with a given particle ID and copy them to a new dataset
+        """
+
+        if not partID in self.partIdx.keys():
+            logger.error("Particle ID %d does not exist in dataset" % partID)
+            return False
+
+        self.fData = None
+        self.fData = {dKey:[] for dKey in self.colNames}
+
+        for cN in self.colNames:
+            self.fData[cN] = self.Data[cN][self.partIdx[partID]]
+
+        logger.info("%d lines were filtered into fData" % len(self.fData["ID"]))
+
+        return True
+
+
+    def filterTurn(self, turnID):
+        """
+        Select all particles with a given turn ID and copy them to a new dataset
+        """
+
+        if not turnID in self.turnIdx.keys():
+            logger.error("Turn ID %d does not exist in dataset" % turnID)
+            return False
+
+        self.fData = None
+        self.fData = {dKey:[] for dKey in self.colNames}
+
+        for cN in self.colNames:
+            self.fData[cN] = self.Data[cN][self.turnIdx[turnID]]
+
+        logger.info("%d lines were filtered into fData" % len(self.fData["ID"]))
+
+        return True
+
+    #
+    #  Internal Functions
+    #
+
     def stripQuotes(self, sVar):
 
-        if (sVar[0] == sVar[-1]) and sVar.startswith(("'", '"')):
+        if (sVar[0] == sVar[-1]) and sVar.startswith(("'",'"')):
             return sVar[1:-1]
 
         return sVar
-    
 
 ## End Class TableFS
-

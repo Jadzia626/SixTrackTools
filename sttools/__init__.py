dd60f386e22a0a286cbb220cc9f282bad86f861c
Veronica K. B. Olsen <code@vkbo.net>
2017-10-05 16:41:48 +0200

    Changes to logging

--- sttools/__init__.py
+++ sttools/__init__.py
@@ -21,22 +21,31 @@ __all__ = ["STDump","Twiss"]
 
 # Package Meta
 __author__     = "Veronica Berglyd Olsen"
-__copyright__  = "Copyright 2017, Veronica Berglyd Olsen"
+__copyright__  = "Copyright 2017, Veronica Berglyd Olsen, CERN BE-ABP-HSS"
 __credits__    = ["Veronica Berglyd Olsen"]
 __license__    = "GPLv3"
 __version__    = "0.1.0"
 __date__       = "2017"
 __maintainer__ = "Veronica Berglyd Olsen"
 __email__      = "v.k.b.olsen@cern.ch"
-__status__     = "Development"
+__status__     = "Perpetual Development"
 
-logging.basicConfig(
-    # format  = "[%(asctime)s] %(name)s:%(lineno)-4d %(levelname)s: %(message)s",
-    format  = "[%(name)s:%(lineno)-4d] %(levelname)s: %(message)s",
-    level   = logging.DEBUG,
-    # datefmt = "%Y-%m-%d %H:%M:%S",
-    datefmt = "%H:%M:%S",
-)
+logLevel = logging.INFO
+
+if logLevel == logging.DEBUG:
+    logging.basicConfig(
+        # format  = "[%(asctime)s] %(name)s:%(lineno)-4d %(levelname)s: %(message)s",
+        format  = "[%(asctime)s] %(name)s:%(lineno)d %(levelname)s: %(message)s",
+        level   = logging.DEBUG,
+        datefmt = "%Y-%m-%d %H:%M:%S",
+    )
+else:
+    logging.basicConfig(
+        # format  = "[%(asctime)s] %(name)s:%(lineno)-4d %(levelname)s: %(message)s",
+        format  = "[%(module)s] %(levelname)s: %(message)s",
+        level   = logLevel,
+        datefmt = "%H:%M:%S",
+    )
 
 if __name__ == '__main__':
     print("SixTrack Tools")

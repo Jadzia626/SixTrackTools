221bd41886c01f5eee9fc9bcb5a280b59a9243b7
Veronica K. B. Olsen <v.k.b.olsen@cern.ch>
2017-10-18 13:18:34 +0200

    Added the rest of the HL-LHC v1.2 parameters

---
+++ sttools/lhc/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*
+"""SixTrack Tools - LHC Parameters Init
+
+  SixTrack Tools - LHC Parameters Init
+ ======================================
+  Module that holds LHC parameters
+  By: Veronica Berglyd Olsen
+      CERN (BE-ABP-HSS)
+      Geneva, Switzerland
+
+"""
+
+import sttools
+
+logger = logging.getLogger(__name__)
--- sttools/lhc/hllhc.py
+++ sttools/lhc/hllhc.py
@@ -34,13 +34,11 @@ class HLLHCv12():
     IP_5 = "CMS"
     IP_8 = "LHCb"
     
-    # Global Parameters
+    # Parameters
     beamEnergy    = [None, None]
     partPerBunch  = [None, None]
     nBunches      = [None, None]
     normEmittance = [None, None]
-    
-    # Config Specific
     betaStarX     = [None, None]
     betaStarY     = [None, None]
     posX          = [None, None]
@@ -86,10 +84,180 @@ class HLLHCv12():
             else:
                 logger.error("Unknown interaction point encountered")
         elif machineConfig == self.CONF_PRE_SQUEEZE:
+            self.beamEnergy    = [ 7000.00e9 , 7000.00e9 ]
+            self.partPerBunch  = [    2.20e11,    2.20e11]
+            self.nBunches      = [ 2748      , 2748      ]
+            self.normEmittance = [    2.50e-6,    2.50e-6]
+            if interactionPoint == self.IP_1:
+                self.betaStarX = [  480.00e-3,  480.00e-3]
+                self.betaStarY = [  480.00e-3,  480.00e-3]
+                self.posX      = [   -2.00e-3,    2.00e-3]
+                self.posY      = [    0.00e-3,    0.00e-3]
+                self.crossingX = [    0.00e-6,    0.00e-6]
+                self.crossingY = [  295.00e-6, -295.00e-6]
+            elif interactionPoint == self.IP_2:
+                self.betaStarX = [   10.00   ,   10.00   ]
+                self.betaStarY = [   10.00   ,   10.00   ]
+                self.posX      = [    2.00e-3,   -2.00e-3]
+                self.posY      = [    0.00e-3,    0.00e-3]
+                self.crossingX = [  290.00e-9, -290.00e-9]
+                self.crossingY = [  240.00e-6, -240.00e-6]
+            elif interactionPoint == self.IP_5:
+                self.betaStarX = [  480.00e-3,  480.00e-3]
+                self.betaStarY = [  480.00e-3,  480.00e-3]
+                self.posX      = [    0.00e-3,    0.00e-3]
+                self.posY      = [    2.00e-3,   -2.00e-3]
+                self.crossingX = [  295.00e-6, -295.00e-6]
+                self.crossingY = [    0.00e-6,    0.00e-6]
+            elif interactionPoint == self.IP_8:
+                self.betaStarX = [    3.00   ,    3.00   ]
+                self.betaStarY = [    3.00   ,    3.00   ]
+                self.posX      = [    0.00e-3,    0.00e-3]
+                self.posY      = [   -2.00e-3,    2.00e-3]
+                self.crossingX = [ -415.00e-6,  415.00e-6]
+                self.crossingY = [   -1.81e-6,    1.81e-6]
+            else:
+                logger.error("Unknown interaction point encountered")
         elif machineConfig == self.CONF_COLLISION_ROUND:
+            self.beamEnergy    = [ 7000.00e9 , 7000.00e9 ]
+            self.partPerBunch  = [    2.20e11,    2.20e11]
+            self.nBunches      = [ 2748      , 2748      ]
+            self.normEmittance = [    2.50e-6,    2.50e-6]
+            if interactionPoint == self.IP_1:
+                self.betaStarX = [  150.00e-3,  150.00e-3]
+                self.betaStarY = [  150.00e-3,  150.00e-3]
+                self.posX      = [ -750.00e-6,  750.00e-6]
+                self.posY      = [    0.00e-3,    0.00e-3]
+                self.crossingX = [    0.00e-6,    0.00e-6]
+                self.crossingY = [  295.00e-6, -295.00e-6]
+            elif interactionPoint == self.IP_2:
+                self.betaStarX = [   10.00   ,   10.00   ]
+                self.betaStarY = [   10.00   ,   10.00   ]
+                self.posX      = [    2.00e-3,   -2.00e-3]
+                self.posY      = [    0.00e-3,    0.00e-3]
+                self.crossingX = [  290.00e-9, -290.00e-9]
+                self.crossingY = [  240.00e-6, -240.00e-6]
+            elif interactionPoint == self.IP_5:
+                self.betaStarX = [  150.00e-3,  150.00e-3]
+                self.betaStarY = [  150.00e-3,  150.00e-3]
+                self.posX      = [    0.00e-3,    0.00e-3]
+                self.posY      = [  750.00e-6, -750.00e-6]
+                self.crossingX = [  295.00e-6, -295.00e-6]
+                self.crossingY = [    0.00e-6,    0.00e-6]
+            elif interactionPoint == self.IP_8:
+                self.betaStarX = [    3.00   ,    3.00   ]
+                self.betaStarY = [    3.00   ,    3.00   ]
+                self.posX      = [    0.00e-3,    0.00e-3]
+                self.posY      = [   -2.00e-3,    2.00e-3]
+                self.crossingX = [ -415.00e-6,  415.00e-6]
+                self.crossingY = [   -1.81e-6,    1.81e-6]
+            else:
+                logger.error("Unknown interaction point encountered")
         elif machineConfig == self.CONF_COLLISION_FLAT:
+            self.beamEnergy    = [ 7000.00e9 , 7000.00e9 ]
+            self.partPerBunch  = [    2.20e11,    2.20e11]
+            self.nBunches      = [ 2748      , 2748      ]
+            self.normEmittance = [    2.50e-6,    2.50e-6]
+            if interactionPoint == self.IP_1:
+                self.betaStarX = [   75.00e-3,   75.00e-3]
+                self.betaStarY = [  300.00e-3,  300.00e-3]
+                self.posX      = [ -750.00e-6,  750.00e-6]
+                self.posY      = [    0.00e-3,    0.00e-3]
+                self.crossingX = [    0.00e-6,    0.00e-6]
+                self.crossingY = [  275.00e-6, -275.00e-6]
+            elif interactionPoint == self.IP_2:
+                self.betaStarX = [   10.00   ,   10.00   ]
+                self.betaStarY = [   10.00   ,   10.00   ]
+                self.posX      = [    2.00e-3,   -2.00e-3]
+                self.posY      = [    0.00e-3,    0.00e-3]
+                self.crossingX = [  290.00e-9, -290.00e-9]
+                self.crossingY = [  240.00e-6, -240.00e-6]
+            elif interactionPoint == self.IP_5:
+                self.betaStarX = [  300.00e-3,  300.00e-3]
+                self.betaStarY = [   75.00e-3,   75.00e-3]
+                self.posX      = [    0.00e-3,    0.00e-3]
+                self.posY      = [  750.00e-6, -750.00e-6]
+                self.crossingX = [  275.00e-6, -275.00e-6]
+                self.crossingY = [    0.00e-6,    0.00e-6]
+            elif interactionPoint == self.IP_8:
+                self.betaStarX = [    3.00   ,    3.00   ]
+                self.betaStarY = [    3.00   ,    3.00   ]
+                self.posX      = [    0.00e-3,    0.00e-3]
+                self.posY      = [   -2.00e-3,    2.00e-3]
+                self.crossingX = [ -415.00e-6,  415.00e-6]
+                self.crossingY = [   -1.81e-6,    1.81e-6]
+            else:
+                logger.error("Unknown interaction point encountered")
         elif machineConfig == self.CONF_VDM_30:
+            self.beamEnergy    = [ 7000.00e9 , 7000.00e9 ]
+            self.partPerBunch  = [    2.20e11,    2.20e11]
+            self.nBunches      = [ 2748      , 2748      ]
+            self.normEmittance = [    2.50e-6,    2.50e-6]
+            if interactionPoint == self.IP_1:
+                self.betaStarX = [   30.00   ,   30.00   ]
+                self.betaStarY = [   30.00   ,   30.00   ]
+                self.posX      = [ -750.00e-6,  750.00e-6]
+                self.posY      = [    0.00e-3,    0.00e-3]
+                self.crossingX = [    0.00e-6,    0.00e-6]
+                self.crossingY = [  295.00e-6, -295.00e-6]
+            elif interactionPoint == self.IP_2:
+                self.betaStarX = [   30.00   ,   30.00   ]
+                self.betaStarY = [   30.00   ,   30.00   ]
+                self.posX      = [    2.00e-3,   -2.00e-3]
+                self.posY      = [    0.00e-3,    0.00e-3]
+                self.crossingX = [  300.00e-9, -290.00e-9]
+                self.crossingY = [  240.00e-6, -240.00e-6]
+            elif interactionPoint == self.IP_5:
+                self.betaStarX = [   30.00   ,   30.00   ]
+                self.betaStarY = [   30.00   ,   30.00   ]
+                self.posX      = [ -750.00e-6,  750.00e-6]
+                self.posY      = [  750.00e-6, -750.00e-6]
+                self.crossingX = [  295.00e-6, -295.00e-6]
+                self.crossingY = [  295.00e-6, -295.00e-6]
+            elif interactionPoint == self.IP_8:
+                self.betaStarX = [   30.00   ,   30.00   ]
+                self.betaStarY = [   30.00   ,   30.00   ]
+                self.posX      = [    0.00e-3,    0.00e-3]
+                self.posY      = [   -2.00e-3,    2.00e-3]
+                self.crossingX = [ -305.00e-6,  305.00e-6]
+                self.crossingY = [   -1.80e-6,    1.80e-6]
+            else:
+                logger.error("Unknown interaction point encountered")
         elif machineConfig == self.CONF_LOW_BETA:
+            self.beamEnergy    = [ 7000.00e9 , 7000.00e9 ]
+            self.partPerBunch  = [    2.20e11,    2.20e11]
+            self.nBunches      = [ 2748      , 2748      ]
+            self.normEmittance = [    2.50e-6,    2.50e-6]
+            if interactionPoint == self.IP_1:
+                self.betaStarX = [  480.00e-3,  480.00e-3]
+                self.betaStarY = [  480.00e-3,  480.00e-3]
+                self.posX      = [   -2.00e-3,    2.00e-3]
+                self.posY      = [    0.00e-3,    0.00e-3]
+                self.crossingX = [    0.00e-6,    0.00e-6]
+                self.crossingY = [  295.00e-6, -295.00e-6]
+            elif interactionPoint == self.IP_2:
+                self.betaStarX = [  500.00e-3,  500.00e-3]
+                self.betaStarY = [  500.00e-3,  500.00e-3]
+                self.posX      = [    2.00e-3,   -2.00e-3]
+                self.posY      = [    0.00e-3,    0.00e-3]
+                self.crossingX = [  290.00e-9, -290.00e-9]
+                self.crossingY = [  240.00e-6, -240.00e-6]
+            elif interactionPoint == self.IP_5:
+                self.betaStarX = [  480.00e-3,  480.00e-3]
+                self.betaStarY = [  480.00e-3,  480.00e-3]
+                self.posX      = [    0.00e-3,    0.00e-3]
+                self.posY      = [    2.00e-3,   -2.00e-3]
+                self.crossingX = [  295.00e-6, -295.00e-6]
+                self.crossingY = [    0.00e-6,    0.00e-6]
+            elif interactionPoint == self.IP_8:
+                self.betaStarX = [  500.00e-3,  500.00e-3]
+                self.betaStarY = [  500.00e-3,  500.00e-3]
+                self.posX      = [    0.00e-3,    0.00e-3]
+                self.posY      = [   -2.00e-3,    2.00e-3]
+                self.crossingX = [ -305.00e-6,  305.00e-6]
+                self.crossingY = [   -1.81e-6,    1.81e-6]
+            else:
+                logger.error("Unknown interaction point encountered")
         else:
             logger.error("Unknown machine config encountered")
         

d64fd496e9b8d15f4b3ab111c59ee49e969d7bf2
Veronica K. B. Olsen <v.k.b.olsen@cern.ch>
2018-01-12 19:35:17 +0100

    Added code to Aperture class

--- sttools/__init__.py
+++ sttools/__init__.py
@@ -14,25 +14,26 @@
 import logging
 
 # Submodules
-from .stdump  import STDump
-from .tablefs import TableFS
-from .twiss   import Twiss
+from .aperture import Aperture
+from .stdump   import STDump
+from .tablefs  import TableFS
+from .twiss    import Twiss
 
-logger = logging.getLogger(__name__)
-
-__all__ = ["STDump","Twiss"]
+__all__ = ["Aperture","STDump","TableFS","Twiss"]
 
 # Package Meta
 __author__     = "Veronica Berglyd Olsen"
-__copyright__  = "Copyright 2017, Veronica Berglyd Olsen, CERN BE-ABP-HSS"
-__credits__    = ["Veronica Berglyd Olsen"]
+__copyright__  = "Copyright 2017-2018, Veronica Berglyd Olsen, CERN (BE-ABP-HSS)"
+__credits__    = ["Veronica Berglyd Olsen","Kyrre Ness Sjøbæk","Andrea Santamaria Garcia"]
 __license__    = "GPLv3"
 __version__    = "0.1.0"
-__date__       = "2017"
+__date__       = "2018"
 __maintainer__ = "Veronica Berglyd Olsen"
 __email__      = "v.k.b.olsen@cern.ch"
 __status__     = "Perpetual Development"
 
+# Logging
+logger   = logging.getLogger(__name__)
 logLevel = logging.INFO
 
 if logLevel == logging.DEBUG:
@@ -48,5 +49,6 @@ else:
         datefmt = "%H:%M:%S",
     )
 
+# Fallback
 if __name__ == '__main__':
     print("SixTrack Toolbox for Python 3")
--- sttools/aperture.py
+++ sttools/aperture.py
@@ -15,14 +15,78 @@ import numpy   as np
 import re
 
 from os import path
+from .tablefs import TableFS
 
 logger = logging.getLogger(__name__)
 
 class Aperture:
     
+    fileName   = None
+    fileLoaded = None
+    
+    aperData   = None
+    aperS      = None # Position along the beam line
+    aperX      = None # Horisontal aperture limits
+    aperY      = None # Vertical aperture limits
+    aperN      = None # Number of elements
+    
     def __init__(self, fileName):
         
-        self.fileName = fileName
+        self.fileLoaded = False
+        self.filterOpts = {
+            
+        }
+        
+        if path.isfile(fileName):
+            logger.info("Loading file %s" % path.basename(fileName))
+        else:
+            logger.error("File not found %s" % fileName)
+            return
+        
+        self.fileName   = fileName
+        self.aperData   = TableFS(self.fileName)
+        self.fileLoaded = True
+        
+        return
+    
+    def parseApertures(self):
+        
+        # Pre-allocate arrays
+        self.aperN = self.aperData.nLines
+        self.aperS = np.zeros(self.aperN)
+        self.aperX = np.zeros(self.aperN)
+        self.aperY = np.zeros(self.aperN)
+        
+        for n in range(self.aperN):
+            
+            aperName = self.aperData.Data["NAME"][n]
+            aperKeyw = self.aperData.Data["KEYWORD"][n]
+            aperType = self.aperData.Data["APERTYPE"][n]
+            
+            self.aperS[n] = self.aperData.Data["S"][n]
+            
+            if   aperType == "CIRCLE":
+                self.aperX[n] = self.aperData.Data["APER_1"][n]
+                self.aperY[n] = self.aperData.Data["APER_1"][n]
+            elif aperType == "RECTANGLE":
+                self.aperX[n] = self.aperData.Data["APER_1"][n]
+                self.aperY[n] = self.aperData.Data["APER_2"][n]
+            elif aperType == "ELLIPSE":
+                self.aperX[n] = self.aperData.Data["APER_1"][n]
+                self.aperY[n] = self.aperData.Data["APER_2"][n]
+            elif aperType == "RECTELLIPSE":
+                self.aperX[n] = min(self.aperData.Data["APER_1"][n], self.aperData.Data["APER_3"][n])
+                self.aperY[n] = min(self.aperData.Data["APER_2"][n], self.aperData.Data["APER_4"][n])
+            elif aperType == "RACETRACK":
+                self.aperX[n] = self.aperData.Data["APER_1"][n] + self.aperData.Data["APER_3"][n]
+                self.aperY[n] = self.aperData.Data["APER_2"][n] + self.aperData.Data["APER_4"][n]
+            elif aperType == "NONE":
+                self.aperX[n] = 0.0
+                self.aperY[n] = 0.0
+            else:
+                logger.warning("Unhandled APERTYPE '%s'" % aperType)
+                self.aperX[n] = 9.999
+                self.aperY[n] = 9.999
         
         return
     

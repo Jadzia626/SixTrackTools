59335111539da4b67cbd478f8cc971d40f92aff2
Veronica K. B. Olsen <v.k.b.olsen@cern.ch>
2018-05-31 14:34:23 +0200

    Added some more code to the new version of concatenator class

--- sttools/concatenator.py
+++ sttools/concatenator.py
@@ -14,23 +14,45 @@ import logging
 import numpy   as np
 import h5py
 
-from os import path
-
-from .tablefs import TableFS
-from .stdump  import STDump
+from os import path,listdir
 
 logger = logging.getLogger(__name__)
 
 class Concatenator:
     
-    def __init__(self, inFolder, fileFilter=None):
+    def __init__(self, inFolder):
         
         if not path.isdir(inFolder):
             logger.error("Path not found: %s" % inFolder)
             return
         
-        
+        self.inFolder = inFolder
+        self.fileList = []
         
         return
+    
+    def loadAll(self, theExt=None):
+        
+        fileList = listdir(self.inFolder)
+        if theExt is None:
+            extList = (".hdf5",".h5",".hdf")
+        else:
+            extList = (theExt)
+        
+        for elem in fileList:
+            fName, fExt = path.splitext(elem)
+            if fExt in extList:
+                self.fileList.append(elem)
+        
+        return True
+    
+    def extractAppend(self, dataSet, dataCol):
+        
+        for inFile in self.fileList:
+            filePath = path.join(self.inFolder,inFile)
+            h5File = h5py.File(filePath, mode="r")
+            
+        
+        return True
     
 # End Class Concatenator
---
+++ tests/concat/test_concat.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*
+"""Test Script for Concatenator Class
+  
+  SixTrack Tools - Test Script for Concatenator Class
+ =====================================================
+  By: Veronica Berglyd Olsen
+      CERN (BE-ABP-HSS)
+      Geneva, Switzerland
+"""
+
+import filecmp as fcmp
+
+from os      import path, unlink
+from shutil  import copyfile
+
+from sttools import Concatenator
+
+currPath = path.dirname(path.realpath(__file__))
+
+copyfile(path.join(currPath,"data.hdf5"),path.join(currPath,"test.001.hdf5"))
+copyfile(path.join(currPath,"data.hdf5"),path.join(currPath,"test.002.hdf5"))
+copyfile(path.join(currPath,"data.hdf5"),path.join(currPath,"test.003.hdf5"))
+
+fCC = Concatenator(currPath)
+
+def testLoadFiles():
+    assert fCC.loadAll()
+    assert len(set(fCC.fileList).intersection(
+        ["data.hdf5","test.001.hdf5","test.002.hdf5","test.003.hdf5"]
+    )) == 4
+    # assert False
+
+# def testLoadDumpFile():
+#     assert fCC.appendParticles(path.join(currPath,"dump_ip5.dat"),Concatenator.FTYPE_PART_DUMP)
+#     assert fCC.appendParticles(path.join(currPath,"dump_ip5.dat"),Concatenator.FTYPE_PART_DUMP)
+
+# def testCloseFile():
+#     assert fCC.closeFile()
+
+# Checksum does not work for comparing HDF5 files.
+# def testFileChecksum():
+#     md5Sum = "2cb5b90811acc27dcfa3de80c3e3800c"
+#     assert md5(open(path.join(currPath,"test.hdf5"),mode="rb").read()).hexdigest() == md5Sum
+
+# unlink(path.join(currPath,"test.001.hdf5"))
+# unlink(path.join(currPath,"test.002.hdf5"))
+# unlink(path.join(currPath,"test.003.hdf5"))

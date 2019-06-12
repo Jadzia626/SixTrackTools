8529030dedb085ed10aa0b776e3c6e586518a6bd
Veronica K. B. Olsen <v.k.b.olsen@cern.ch>
2019-06-12 10:49:41 +0200

    Updated Fort2 class to support multicol fort.2 file

--- sttools/__init__.py
+++ sttools/__init__.py
@@ -21,12 +21,13 @@ from sttools.dataset    import DataSet
 __all__ = ["Const","Physics","SixTrackSim","DataSet"]
 
 # Package Meta
+__package__    = "SixTrackTools"
 __author__     = "Veronica Berglyd Olsen"
-__copyright__  = "Copyright 2017-2018, Veronica Berglyd Olsen, CERN (BE-ABP-HSS)"
+__copyright__  = "Copyright 2017-2019, Veronica Berglyd Olsen, CERN (BE-ABP-HSS)"
 __credits__    = ["Veronica Berglyd Olsen","Kyrre Ness Sjøbæk","Andrea Santamaria Garcia"]
 __license__    = "GPLv3"
 __version__    = "0.1.0"
-__date__       = "2018"
+__date__       = "2019"
 __maintainer__ = "Veronica Berglyd Olsen"
 __email__      = "v.k.b.olsen@cern.ch"
 __status__     = "Perpetual Development"
--- sttools/simtools/fort2.py
+++ sttools/simtools/fort2.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*
+# -*- coding: utf-8 -*-
 """Fort.2 File Wrapper
 
   SixTrack Tools - Fort.2 File Wrapper
@@ -19,11 +19,12 @@ from os import path
 logger = logging.getLogger(__name__)
 
 class Fort2():
-    
+
     filePath   = None
     fileName   = None
     fileExists = None
-    
+    isMultiCol = False
+
     elemData   = {
         "Name" : [],
         "Type" : [],
@@ -38,40 +39,45 @@ class Fort2():
         "Block" : [],
         "Drift" : [],
     }
-    structData = []
-    
+    structDataS = []
+    structDataM = {
+        "Full" : [],
+        "Name" : [],
+        "SPos" : [],
+    }
+
     def __init__(self, filePath, fileName="fort.2"):
-        
+
         # Set constants and defaults
         self.nameElem   = "SINGLE ELEMENTS"
         self.nameBlock  = "BLOCK DEFINITIONS"
         self.nameStruct = "STRUCTURE INPUT"
-        
+
         self.fileExists = False
-        
+
         if path.isdir(filePath):
             self.filePath = filePath
         else:
             logger.error("Path not found: %s", filePath)
-        
+
         if path.isfile(path.join(filePath,fileName)):
             self.fileName   = fileName
             self.fileExists = True
         else:
             logger.error("Found no %s file in path", fileName)
-        
+
         return
-    
+
     def loadFile(self):
-        
+
         if not self.fileExists:
             logger.error("No valid file specified")
             return False
-        
+
         fort2File = path.join(self.filePath,self.fileName)
         whatStage = 0
         whatLine  = 0
-        
+
         with open(fort2File,"r") as inFile:
             whatLine += 1
             for theLine in inFile:
@@ -87,9 +93,9 @@ class Fort2():
                     whatStage = 3
                     logger.info("Parsing structure inputs ...")
                     continue
-                elif theLine[0:4]  == "NEXT":
+                elif theLine[0:4] == "NEXT":
                     whatStage = 0
-                
+
                 # This is a temporary fix while waiting for MadX Pull Request #552 to be merged
                 theLine   = theLine.replace("   3   1.000000000e-08","   0   0.000000000e+00")
                 lineElems = theLine.split()
@@ -112,34 +118,49 @@ class Fort2():
                     else:
                         logger.warning("Line %d has the wrong number of elements" % whatLine)
                 elif whatStage == 3:
-                    for e in range(len(lineElems)):
-                        self.structData.append(lineElems[e])
-        
-        self.elemData["Name"]   = np.asarray(self.elemData["Name"],  dtype="str")
-        self.elemData["Type"]   = np.asarray(self.elemData["Type"],  dtype="int")
-        self.elemData["Val1"]   = np.asarray(self.elemData["Val1"],  dtype="float")
-        self.elemData["Val2"]   = np.asarray(self.elemData["Val2"],  dtype="float")
-        self.elemData["Val3"]   = np.asarray(self.elemData["Val3"],  dtype="float")
-        self.elemData["Val4"]   = np.asarray(self.elemData["Val4"],  dtype="float")
-        self.elemData["Val5"]   = np.asarray(self.elemData["Val5"],  dtype="float")
-        self.elemData["Val6"]   = np.asarray(self.elemData["Val6"],  dtype="float")
-        self.blockData["Block"] = np.asarray(self.blockData["Block"],dtype="str")
-        self.blockData["Drift"] = np.asarray(self.blockData["Drift"],dtype="str")
-        self.structData         = np.asarray(self.structData[:],     dtype="str")
-        
+                    if len(lineElems) == 1:
+                        if lineElems[0] == "MULTICOL":
+                            self.isMultiCol = True
+                            continue
+                    if self.isMultiCol:
+                        if len(lineElems) >= 3:
+                            self.structDataM["Full"].append(lineElems[0])
+                            self.structDataM["Name"].append(lineElems[1])
+                            self.structDataM["SPos"].append(lineElems[2])
+                        else:
+                            logger.warning("Line %d has the wrong number of elements" % whatLine)
+                    else:
+                        for e in range(len(lineElems)):
+                            self.structDataS.append(lineElems[e])
+
+        self.elemData["Name"]    = np.asarray(self.elemData["Name"],   dtype="str")
+        self.elemData["Type"]    = np.asarray(self.elemData["Type"],   dtype="int")
+        self.elemData["Val1"]    = np.asarray(self.elemData["Val1"],   dtype="float")
+        self.elemData["Val2"]    = np.asarray(self.elemData["Val2"],   dtype="float")
+        self.elemData["Val3"]    = np.asarray(self.elemData["Val3"],   dtype="float")
+        self.elemData["Val4"]    = np.asarray(self.elemData["Val4"],   dtype="float")
+        self.elemData["Val5"]    = np.asarray(self.elemData["Val5"],   dtype="float")
+        self.elemData["Val6"]    = np.asarray(self.elemData["Val6"],   dtype="float")
+        self.blockData["Block"]  = np.asarray(self.blockData["Block"], dtype="str")
+        self.blockData["Drift"]  = np.asarray(self.blockData["Drift"], dtype="str")
+        self.structDataS         = np.asarray(self.structDataS[:],     dtype="str")
+        self.structDataM["Full"] = np.asarray(self.structDataM["Full"],dtype="str")
+        self.structDataM["Name"] = np.asarray(self.structDataM["Name"],dtype="str")
+        self.structDataM["SPos"] = np.asarray(self.structDataM["SPos"],dtype="float")
+
         return True
-    
+
     def saveFile(self, savePath=None, saveFile="fort.2"):
-        
+
         if savePath is None:
             savePath = self.filePath
-        
+
         if not path.isdir(savePath):
             logger.error("Path not found: %s" & savePath)
             return False
-        
+
         with open(path.join(savePath,saveFile),"w") as outFile:
-            
+
             # Writing Single Elements
             outFile.write(self.nameElem+"-"*57+"\n")
             for e in range(len(self.elemData["Name"])):
@@ -154,7 +175,7 @@ class Fort2():
                     self.elemData["Val6"][e],
                 ))
             outFile.write("NEXT\n")
-            
+
             # Writing Block Definitions
             outFile.write(self.nameBlock+"-"*55+"\n")
             for e in range(len(self.blockData["Block"])):
@@ -163,19 +184,28 @@ class Fort2():
                     self.blockData["Drift"][e],
                 ))
             outFile.write("NEXT\n")
-            
+
             # Writing Structure Input
             outFile.write(self.nameStruct+"-"*57+"\n")
-            for e in range(len(self.structData)):
-                outFile.write(("{:<24s} ").format(self.structData[e]))
-                if e > 0 and e%3 == 2:
+            if self.isMultiCol:
+                outFile.write("MULTICOL\n")
+                for e in range(len(self.structDataM["Full"])):
+                    outFile.write(("{:<24s} {:<24s} {:17.9f}\n").format(
+                        self.structDataM["Full"][e],
+                        self.structDataM["Name"][e],
+                        self.structDataM["SPos"][e],
+                    ))
+            else:
+                for e in range(len(self.structDataS)):
+                    outFile.write(("{:<24s} ").format(self.structDataS[e]))
+                    if e > 0 and e%3 == 2:
+                        outFile.write("\n")
+                if not e%3 == 2:
                     outFile.write("\n")
-            if not e%3 == 2:
-                outFile.write("\n")
             outFile.write("NEXT\n")
-        
+
         return True
-        
+
     def insertElement(self, inName, inType, inValues, inRef, inOffset=0):
         """
         Inserts a new marker before the element inRef.
@@ -183,7 +213,7 @@ class Fort2():
         inOffset can be used to offset this, i.e. an inOffset of 1 will insert the
         marker immediately after the inRef point instead of before (inOffset = 0).
         """
-        
+
         if isinstance(inRef,str):
             arrRef, = np.where(self.elemData["Name"] == inRef)
             if len(arrRef) == 1:
@@ -199,20 +229,20 @@ class Fort2():
         else:
             logger.error("inRef must be either string or integer")
             return False
-        
+
         if inRef < 0 or inRef >= len(self.elemData["Name"]):
             logger.error("Index out of bounds")
             return False
-        
+
         inPos = inRef + inOffset
         if inPos < 0 or inPos >= len(self.elemData["Name"]):
             logger.error("Index + offset out of bounds")
             return False
-        
+
         if not len(inValues) == 6:
             logger.error("Value inValues must be an array of six floats")
             return False
-        
+
         self.elemData["Name"] = np.insert(self.elemData["Name"],inPos,inName)
         self.elemData["Type"] = np.insert(self.elemData["Type"],inPos,inType)
         self.elemData["Val1"] = np.insert(self.elemData["Val1"],inPos,inValues[0])
@@ -221,9 +251,9 @@ class Fort2():
         self.elemData["Val4"] = np.insert(self.elemData["Val4"],inPos,inValues[3])
         self.elemData["Val5"] = np.insert(self.elemData["Val5"],inPos,inValues[4])
         self.elemData["Val6"] = np.insert(self.elemData["Val6"],inPos,inValues[5])
-        
+
         return True
-    
+
     def insertStruct(self, inName, inRef, inOffset=0):
         """
         Inserts a new marker before the structure inRef.
@@ -231,7 +261,7 @@ class Fort2():
         inOffset can be used to offset this, i.e. an inOffset of 1 will insert the
         marker immediately after the inRef point instead of before (inOffset = 0).
         """
-        
+
         if isinstance(inRef,str):
             arrRef, = np.where(self.structData == inRef)
             if len(arrRef) == 1:
@@ -247,18 +277,18 @@ class Fort2():
         else:
             logger.error("inRef must be either string or integer")
             return False
-        
+
         if inRef < 0 or inRef >= len(self.structData):
             logger.error("Index out of bounds")
             return False
-        
+
         inPos = inRef + inOffset
         if inPos < 0 or inPos >= len(self.structData):
             logger.error("Index + offset out of bounds")
             return False
-        
+
         self.structData = np.insert(self.structData,inPos,inName)
-        
+
         return True
-        
+
 # END Fort2
